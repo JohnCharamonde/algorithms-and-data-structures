9b8ecdcdda19c0e6e4eeb04c53e23e6e97d9f66f
JohnCharamonde <john.charamonde@gmail.com>
2020-06-08 15:54:42 -0400

    Completed superbalanced binary tree - O(n) time and memory

--- interview-cake/trees-and-graphs/balanced-binary-tree.js
+++ interview-cake/trees-and-graphs/balanced-binary-tree.js
@@ -23,6 +23,36 @@ class BinaryTreeNode {
 }
 
 function isBalanced(treeRoot) {
-    if(!treeRoot)
+    if(!treeRoot) {
+      return true;
+    }
+
+    let depths = [];
+    let nodes = [[treeRoot, 0]];
+
+    while(nodes.length) {
+      let nodePair = nodes.pop();
+      let node = nodePair[0];
+      let depth = nodePair[1];
+
+      if(!node.left && !node.right) {
+        if(depths.indexOf(depth) < 0) {
+          depths.push(depth);
+        }
+
+        if((depths.length > 2) || ( depths.length === 2 && Math.abs(depths[0] - depths[1]) > 1)) {
+          return false;
+        }
+      }
+
+      if(node.left) {
+        nodes.push([node.left, depth + 1]);
+      }
+      if(node.right) {
+        nodes.push([node.right, depth + 1]);
+      }
+    }
+
+  return true;
 }
 
