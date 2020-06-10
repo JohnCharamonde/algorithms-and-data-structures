4bd6117f11a475ae009bc2da520cd1c8f41eae57
JohnCharamonde <john.charamonde@gmail.com>
2020-06-10 19:40:53 -0400

    Impletented function to delete node in linked list. O(1) time and memory...but with side effects!

---
+++ cake/linked-lists/delete-node.js
@@ -0,0 +1,18 @@
+class LinkedListNode {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+
+  // unwanted side effects:
+  // noteToDelete takes over the node at nodeToDelete.next value/next, effectively stranding that node.
+  // 1) references to nodeToDelete now have different value/next than previously set.
+  // 2) references to the node at the old nodeToDelete.next are stranded and won't be part of the linked list if referenced again. Will eventually be garbage collected.
+  function deleteNode(nodeToDelete) {
+    // Delete the input node from the linked list
+    nodeToDelete.value = nodeToDelete.next.value;
+    nodeToDelete.next = nodeToDelete.next.next;
+  }
+  
