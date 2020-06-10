38a700b9269bb9b1c9563eedcc68a8f593d317c5
JohnCharamonde <john.charamonde@gmail.com>
2020-06-10 19:42:31 -0400

    Additional note set to remind that this implementation won't work for the tail of a linked-list

--- cake/linked-lists/delete-node.js
+++ cake/linked-lists/delete-node.js
@@ -10,6 +10,8 @@ class LinkedListNode {
   // noteToDelete takes over the node at nodeToDelete.next value/next, effectively stranding that node.
   // 1) references to nodeToDelete now have different value/next than previously set.
   // 2) references to the node at the old nodeToDelete.next are stranded and won't be part of the linked list if referenced again. Will eventually be garbage collected.
+  // ALSO...
+  // does not work for deleting the tail
   function deleteNode(nodeToDelete) {
     // Delete the input node from the linked list
     nodeToDelete.value = nodeToDelete.next.value;
